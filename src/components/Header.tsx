efa83bec4619a9802de3716eee75c57bad747564
a-sha0234 <asharma_31@hotmail.com>
2023-03-10 18:02:09 +0000

    edit pdf

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,34 +25,34 @@ const Header = () => {
   //   }
   // };
 
-  const generatePDF = (): void => {
-    const content = document.getElementById("content");
+  // const generatePDF = (): void => {
+  //   const content = document.getElementById("content");
 
-    if (content !== null) {
-      html2canvas(content, { scale: 2 }).then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF({
-          orientation: "portrait",
-          unit: "pt",
-          format: "a4",
-        });
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        pdf.addImage(
-          imgData,
-          "PNG",
-          0, // x-coordinate
-          0, // y-coordinate
-          pdfWidth, // width
-          pdfHeight // height
-        );
-        pdf.save("download.pdf");
-      });
-    } else {
-      console.error("Could not find content element");
-    }
-  };
+  //   if (content !== null) {
+  //     html2canvas(content, {scale: 2}).then((canvas) => {
+  //       const imgData = canvas.toDataURL("image/png");
+  //       const pdf = new jsPDF({
+  //         orientation: "portrait",
+  //         unit: "pt",
+  //         format: "a4",
+  //       });
+  //       const imgProps = pdf.getImageProperties(imgData);
+  //       const pdfWidth = pdf.internal.pageSize.getWidth();
+  //       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+  //       pdf.addImage(
+  //         imgData,
+  //         "PNG",
+  //         0, // x-coordinate
+  //         0, // y-coordinate
+  //         pdfWidth, // width
+  //         pdfHeight // height
+  //       );
+  //       pdf.save("download.pdf");
+  //     });
+  //   } else {
+  //     console.error("Could not find content element");
+  //   }
+  // };
 
   // function generatePDF() {
   //   const doc = new jsPDF({ unit: "pt" }); // create jsPDF object
@@ -66,6 +66,32 @@ const Header = () => {
   //     // optional: other HTMLOptions
   //   });
 
+  function printDocument() {
+    const input: any = document.getElementById("content");
+    html2canvas(input, { scrollX: 0, scrollY: -window.scrollY }).then(
+      (canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "pt", [
+          input.offsetWidth,
+          input.offsetHeight,
+        ]);
+
+        // Add image to PDF
+        pdf.addImage(
+          imgData,
+          "PNG",
+          0,
+          0,
+          input.offsetWidth,
+          input.offsetHeight
+        );
+
+        // Download PDF to user
+        pdf.save("resume.pdf");
+      }
+    );
+  }
+
   function handleAutoFill() {}
 
   return (
@@ -75,7 +101,7 @@ const Header = () => {
         Project by <a href="#">fthrt</a>
       </p>
       <button onClick={handleAutoFill}>AutoFill</button>
-      <button onClick={generatePDF}>Save</button>
+      <button onClick={printDocument}>Save</button>
     </>
   );
 };
